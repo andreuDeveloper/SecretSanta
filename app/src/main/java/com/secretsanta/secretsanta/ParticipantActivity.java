841473428075d7d217a556d29429be1a4fb73943
Andrés <arsg93@gmail.com>
2018-01-31 01:36:19 +0100

    Add Participant Activity.

---
+++ app/src/main/java/com/secretsanta/secretsanta/ParticipantActivity.java
@@ -0,0 +1,12 @@
+package com.secretsanta.secretsanta;
+
+import android.support.v4.app.Fragment;
+
+
+public class ParticipantActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new ParticipantFragment();
+    }
+}
---
+++ app/src/main/java/com/secretsanta/secretsanta/ParticipantFragment.java
@@ -0,0 +1,46 @@
+package com.secretsanta.secretsanta;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+
+public class ParticipantFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_participant, container, false);
+        return v;
+    }
+    public void onStart(){
+        super.onStart();
+
+        EditText txtDate=(EditText) getView().findViewById(R.id.txtdate);
+        txtDate.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+            public void onFocusChange(View view, boolean hasfocus){
+                if(hasfocus){
+                    DateDialog dialog=new DateDialog(view);
+                    FragmentTransaction ft =getFragmentManager().beginTransaction();
+                    dialog.show(ft, "DatePicker");
+
+                }
+            }
+
+        });
+    }
+
+
+
+}
